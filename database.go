af4bdcb9e558965cafda1fbb7ce81a0bd4a56f76
cooler-SAI <cooler19860212@yahoo.com>
2024-11-19 22:25:43 +0300

    added sqlite code to repo

---
+++ database.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+const dbName = "expenses.db"
+
+func initDB() *sql.DB {
+	db, err := sql.Open("sqlite3", dbName)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		description TEXT NOT NULL,
+		amount REAL NOT NULL,
+		category TEXT
+	);`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+
+	fmt.Println("Database initialized successfully.")
+	return db
+
+}
