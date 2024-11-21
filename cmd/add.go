82f7c768cc855cfb26af4b6e8187e9a80f824f86
cooler-SAI <cooler19860212@yahoo.com>
2024-11-21 22:05:40 +0300

    added more info into db

--- cmd/add.go
+++ cmd/add.go
@@ -3,11 +3,17 @@ package cmd
 import (
 	"Expense-Tracker/database"
 	"database/sql"
-	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"log"
+	"os"
 	"time"
 )
+
+func initLog() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msgf("DATABASE: Init here...")
+}
 
 var description string
 var amount float64
@@ -22,7 +28,8 @@ var addCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd) // Регистрация команды в rootCmd
+	initLog()
+	rootCmd.AddCommand(addCmd)
 
 	addCmd.Flags().StringVarP(&description, "description", "d", "",
 		"Description of the expense")
@@ -33,37 +40,37 @@ func init() {
 
 	err := addCmd.MarkFlagRequired("description")
 	if err != nil {
-		log.Fatalf("Error marking flag: %v", err)
+		log.Fatal().Msgf("Error marking flag: %v", err)
 	}
 	err2 := addCmd.MarkFlagRequired("amount")
 	if err2 != nil {
-		log.Fatalf("Error marking flag: %v", err)
+		log.Fatal().Msgf("Error marking flag: %v", err)
 	}
 }
 
 func addExpense(description string, amount float64, category string) {
-	log.Printf("addExpense called with: description=%s, amount=%.2f, category=%s", description, amount, category)
+	log.Info().Msgf("addExpense called with: description=%s, amount=%.2f, category=%s", description, amount, category)
 
 	db, err := sql.Open("sqlite3", database.SQLBaseName)
 	if err != nil {
-		log.Fatalf("Error opening database: %v", err)
+		log.Fatal().Msgf("Error opening database: %v", err)
 	}
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
-			log.Fatalf("Error closing database: %v", err)
+			log.Fatal().Msgf("Error closing database: %v", err)
 		}
 	}(db)
 
 	query := `INSERT INTO expenses (date, description, amount, category) VALUES (?, ?, ?, ?)`
-	log.Printf("Executing query: %s", query)
-	log.Printf("Parameters: date=%s, description=%s, amount=%.2f, category=%s",
+	log.Info().Msgf("Executing query: %s", query)
+	log.Info().Msgf("Parameters: date=%s, description=%s, amount=%.2f, category=%s",
 		time.Now().Format("2006-01-02"), description, amount, category)
 
 	_, err = db.Exec(query, time.Now().Format("2006-01-02"), description, amount, category)
 	if err != nil {
-		log.Fatalf("Error adding expense: %v", err)
+		log.Fatal().Msgf("Error adding expense: %v", err)
 	}
 
-	fmt.Printf("Expense added successfully: %s - $%.2f (%s)\n", description, amount, category)
+	log.Info().Msgf("Expense added successfully: %s - $%.2f (%s)\n", description, amount, category)
 }
