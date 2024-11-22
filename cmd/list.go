5145e8fd9fc988b0f95ba793e36fec6275746309
cooler-SAI <cooler19860212@yahoo.com>
2024-11-22 23:36:13 +0300

    set up list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"Expense-Tracker/database"
+	"database/sql"
+	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
 }
 
 var listCmd = &cobra.Command{
@@ -17,7 +18,6 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info().Msg("LIST COMMAND: Starting execution")
 
-		// Инициализация базы данных
 		db := database.GetDB()
 		if db == nil {
 			log.Fatal().Msg("DATABASE: Connection is nil")
@@ -28,4 +28,33 @@ var listCmd = &cobra.Command{
 
 		log.Info().Msg("LIST COMMAND: Finished execution")
 	},
+}
+
+func queryExpenses(db *sql.DB) {
+	var rows, err = db.Query("SELECT date, description, amount, category FROM expenses")
+	if err != nil {
+		log.Fatal().Err(err).Msg("DATABASE: Query failed")
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	fmt.Println("Expenses:")
+	for rows.Next() {
+		var date, description, category string
+		var amount float64
+
+		if err := rows.Scan(&date, &description, &amount, &category); err != nil {
+			log.Fatal().Err(err).Msg("DATABASE: Failed to scan row")
+		}
+
+		fmt.Printf("%s - %s: $%.2f (%s)\n", date, description, amount, category)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal().Err(err).Msg("DATABASE: Error during row iteration")
+	}
 }
